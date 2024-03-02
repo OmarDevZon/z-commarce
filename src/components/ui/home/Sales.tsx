3e7970b764692b3de8072527ac5649b3d9815ce2
omar-web-dev-2 <apoloweb2.0@gmail.com>
2024-03-03 02:45:06 +0600

    Sales

---
+++ src/components/ui/home/Sales.tsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import Image from "../../image";
+import saleImgOne from "../../../assets/image/sale/saleImgOne.webp";
+import saleImgTwo from "../../../assets/image/sale/saleImgTwo.webp";
+import saleImgThree from "../../../assets/image/sale/saleImgThree.webp";
+export const Sales = () => {
+  return (
+    <div className="py-20 flex flex-col md:flex-row items-center justify-between gap-4 lg:gap-10">
+      <div className="w-full md:w-2/3 lg:w-1/2 h-full">
+        <Link to="/shop">
+          <Image className="h-full w-full object-cover" imgSrc={saleImgOne} />
+        </Link>
+      </div>
+      <div className="w-full md:w-2/3 lg:w-1/2 h-auto flex flex-col gap-4 lg:gap-10">
+        <div className="h-1/2 w-full">
+          <Link to="/shop">
+            <Image className="h-full w-full object-cover" imgSrc={saleImgTwo} />
+          </Link>
+        </div>
+        <div className="h-1/2 w-full">
+          <Link to="/shop">
+            <Image
+              className="h-full w-full object-cover"
+              imgSrc={saleImgThree}
+            />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Banner from "../components/ui/home/Banner";
 import { Categories } from "../components/ui/home/Categories";
+import { Sales } from "../components/ui/home/Sales";
 
 export const Home = () => {
   return (
     <>
       <Categories />
       <Banner />
+      <Sales />
     </>
   );
 };
