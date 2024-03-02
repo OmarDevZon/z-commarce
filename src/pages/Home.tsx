de6bf761cbf08a9e2f0f5b48e4a0d8e31a71f7c9
omar-web-dev-2 <apoloweb2.0@gmail.com>
2024-03-03 01:58:55 +0600

    categories

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
+import { Categories } from "../components/ui/home/Categories";
 import { Hero } from "../components/ui/home/hero";
 
 export const Home = () => {
   return (
     <>
+      <Categories />
       <Hero />
     </>
   );
